4a7f2d92b763ed1ab4e65798dade9cb07f059b2a
Katsiaryna Bialova <Katsiaryna.Bialova@thomsonreuters.com>
2018-02-15 18:20:14 +0300

    Update according feature file

--- steps/ApiDocsLoginSteps.java
+++ steps/ApiDocsLoginSteps.java
@@ -1,29 +1,26 @@
 package Homework.Module8.steps;
 
-import Homework.Module8.business_objects.User;
 import Homework.Module8.page.ApiDocsLoginPage;
 import Homework.Module8.page.ApiDocsMainPage;
 import Homework.Module8.patterns.singleton.WebDriverSingleton;
-import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 public class ApiDocsLoginSteps {
     private static WebDriver driver = WebDriverSingleton.getWebDriverInstance();
     private static final String START_URL = "https://amers1.apps.cp.reutest.com/Apps/APIDocs/1.726.1/#/";
 
-    @Given("^user navigates to ApiDocs main page$")
+    @Given("^ApiDocs main page is opened$")
     public void apiDocsLoginPage() {
         driver.get(START_URL);
     }
 
-    @When("^enter Username as \"([^\"]*)\" and Password as \"([^\"]*)\"$")
+    @When("^user enter credentials$")
     public void enter_user_credentials() {
         new ApiDocsLoginPage(driver).login(User);
-
     }
 
     @Then("^ApiDocs main page is displayed$")
